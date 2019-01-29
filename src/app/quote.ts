f5fcd49ef754086f2f66e71c121fab5832fc2f10
UmurerwaDivine <divinekagame@gmail.com>
2019-01-29 17:24:23 +0200

    exta changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
-    public showAuthor:boolean;public showDownVote:boolean;public showUpVote:boolean;
+    public showAuthor:boolean
     constructor(public id:number,public name:string, public author:string, public upvote:number, public downvote:number){
-        this.showAuthor=false;this.showDownVote=true;this.showUpVote=true
+        this.showAuthor=false;
     }
 }
